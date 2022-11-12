aa1cef4e140c44e85fe4eabddf9e8a6107d37eeb
a14a19 <anas31197@gmail.com>
2022-11-12 18:31:24 +0530

    Calc

--- src/App.js
+++ src/App.js
@@ -2,20 +2,36 @@
 import './App.css';
 import Button from './components/button';
 
-let btns = ['%', '+/-', 'c', '/', 7, 8, 9, '*', 4, 5, 6, '-', 1, 2, 3, '+', 0, ',', '=']
+let btns = ['c', '%', '+/-', '/', 7, 8, 9, '*', 4, 5, 6, '-', 1, 2, 3, '+', 0, ',', '=']
 
-// let hex = '0123456789abcdef';
 
+let hex = '0123456789abcdef';
+
+let randomColor = () => {
+  let hexC = '#';
+  for(let x = 0; x < 8; x++){
+    hexC += hex[Math.floor(Math.random() * hex.length)];
+  }
+  return hexC;
+}
+
+let generate = () => {
+  let color1 = randomColor();
+  let color2 = randomColor();
+  
+  return `linear-gradient(${color1}, ${color2})`;
+}
 
 let button = btns.map((btn, i) => {
-  return <Button symbol={btn} key={i}/> 
+  return <Button symbol={btn} key={i} /> 
 })
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ background: generate() }}>
       <div className='calculator'>
         <input type='text' className=''  placeholder='000'/>
+        <div className='r-Display'></div>
         {button}
       </div>
     </div>
--- src/components/button.js
+++ src/components/button.js
@@ -7,11 +7,17 @@ let result = '';
 
 function Button({ symbol }) {
 
-    function getInput(e) {
+    let getInput = (e) => {
         let inputValue = e.target.innerText;
         console.log(inputValue);
-       
-        if (inputValue === '+' || inputValue === '-' || inputValue === '*' || inputValue === '/') {
+        
+        if (inputValue === 'c') {
+            n1 = '';
+            n2 = '';
+            operator = '';
+            result = '';
+        }
+        if (inputValue === '+' || inputValue === '-' || inputValue === '*' || inputValue === '/' || inputValue === '%') {
             operator = inputValue;
         } else if (operator !== '' && inputValue !== '=') {
             n2 += inputValue;
@@ -19,20 +25,13 @@ function Button({ symbol }) {
             n1 += inputValue;
         } else if (inputValue === '=') {
             result = eval(`${+n1} ${operator} ${+n2}`)
-            console.log(result);
-            n1 = '';
+            n1 = result;
             n2 = '';
-            operator = '';
-            result = '';
-        } else if (inputValue === 'c') {
-            n1 = '';
-            n2 = '';
-            operator = '';
-            result = '';
         }
-
         console.log(`n1 (${n1}) ${operator} n2 (${n2}) = ${result}`);
+        console.log(result);
     }
+    
     return (
         <button
             className={symbol === "=" ? 'equal' : 'button'}
