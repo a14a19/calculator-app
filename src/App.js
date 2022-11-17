e3697b054543352c933ca8c8379cd4975305cef6
a14a19 <anas31197@gmail.com>
2022-11-17 19:28:36 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
 import Button from './components/button';
+import { useState } from 'react';
 
-let btns = ['c', '%', '+/-', '/', 7, 8, 9, '*', 4, 5, 6, '-', 1, 2, 3, '+', 0, ',', '=']
+let btns = ['C', '%', '+/-', '/', 7, 8, 9, '*', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '=']
 
 
 let hex = '0123456789abcdef';
 
 let randomColor = () => {
   let hexC = '#';
-  for(let x = 0; x < 8; x++){
+  for (let x = 0; x < 8; x++) {
     hexC += hex[Math.floor(Math.random() * hex.length)];
   }
   return hexC;
@@ -18,21 +19,28 @@ let randomColor = () => {
 let generate = () => {
   let color1 = randomColor();
   let color2 = randomColor();
-  
+
   return `linear-gradient(${color1}, ${color2})`;
 }
 
-let button = btns.map((btn, i) => {
-  return <Button symbol={btn} key={i} /> 
-})
-
 function App() {
+  const [result, setResult] = useState('')
+
+  const showOutput = (output) => {
+    setResult(output);
+  }
+
+  let button = btns.map((btn, i) => {
+    return <Button symbol={btn} key={i} showResult={showOutput} />
+  })
+
   return (
     <div className="App" style={{ background: generate() }}>
       <div className='calculator'>
-        <input type='text' className=''  placeholder='000'/>
-        <div className='r-Display'></div>
-        {button}
+        <input type='text' value={result} placeholder='0' disabled />
+        <div className='btn-container'>
+          {button}
+        </div>
       </div>
     </div>
   );
--- src/components/button.js
+++ src/components/button.js
@@ -5,33 +5,48 @@ let n2 = '';
 let operator = '';
 let result = '';
 
-function Button({ symbol }) {
+function Button({ symbol, showResult }) {
 
     let getInput = (e) => {
         let inputValue = e.target.innerText;
-        console.log(inputValue);
-        
-        if (inputValue === 'c') {
+
+        if (inputValue === 'C') {
             n1 = '';
             n2 = '';
             operator = '';
             result = '';
+            showResult('');
         }
         if (inputValue === '+' || inputValue === '-' || inputValue === '*' || inputValue === '/' || inputValue === '%') {
             operator = inputValue;
-        } else if (operator !== '' && inputValue !== '=') {
+            showResult(`${n1} ${operator} ${n2}`)
+        }
+        else if (operator !== '' && inputValue !== '=') {
             n2 += inputValue;
-        } else if(inputValue !== '=' && inputValue !== 'c'){
+            showResult(`${n1} ${operator} ${n2}`)
+        }
+        else if (inputValue !== '=' && inputValue !== 'C') {
             n1 += inputValue;
-        } else if (inputValue === '=') {
-            result = eval(`${+n1} ${operator} ${+n2}`)
-            n1 = result;
-            n2 = '';
+            showResult(`${n1} ${operator} ${n2}`)
+        }
+        else if (inputValue === '=') {
+            if (inputValue === '.') {
+                result = eval(`${+n1} ${operator} ${+n2}`).toFixed(2);
+                showResult(result);
+                n1 = result;
+                n2 = '';
+            }
+            else {
+                result = eval(`${+n1} ${operator} ${+n2}`)
+                showResult(result);
+                n1 = result;
+                n2 = '';
+            }
         }
         console.log(`n1 (${n1}) ${operator} n2 (${n2}) = ${result}`);
         console.log(result);
     }
-    
+
     return (
         <button
             className={symbol === "=" ? 'equal' : 'button'}
